3d7e935a3207508564caa3cd05e36fa1342f5114
Thinh H Vo <vohongthinh2011@gmail.com>
2016-07-28 17:42:53 -0500

    test

--- app/database/migrations/2016_07_11_175140_create_users_table.php
+++ app/database/migrations/2016_07_11_175140_create_users_table.php
@@ -26,7 +26,7 @@ class CreateUsersTable extends Migration {
 			$table->enum('gender', ['male', 'female']);
 			$table->string('verification_question', 255);
 			$table->string('verifiation_answer', 255);
-			$table->rememberToken();
+			//$table->rememberToken();
 			$table->timestamps();
 			
 
