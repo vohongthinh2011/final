0fc8b044db085f21f74adc3105153fdc28c505a5
Thinh H Vo <vohongthinh2011@gmail.com>
2016-08-02 18:06:09 -0500

    css js

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -4,25 +4,41 @@
 <title>Cinemaphile | Sign Up</title>
 @stop
 
+@section('style')
+<link rel="stylesheet" type="text/css" href="/css/signup.css">
+@stop
+
 @section('content')
-<div>
-	<h1>Sign Up Form 12345</h1>
+<div class="container">
+	<h1 align="center">Sign Up Form</h1>
+	<p align="center">Sign up an new account free and join our community!</p>
+
 	{{ Form::open(['action'=> 'RegistrationController@signUp', 'method' => 'POST']) }}
 
+		<div class="form-group fullName">
 		{{ Form::text('full_name', null, [ 'placeholder' => 'Name', 'required']) }}
-		<br>
+		</div>
+
+		<div class="form-group userName">
 		{{ Form::text('username', null, [ 'placeholder' => 'Username', 'required']) }}
-		<br>
+		</div>
+
+		<div class="form-group email">
 		{{ Form::email('email', null, [ 'placeholder' => 'Email', 'required']) }}
-		<br>
+		</div>
+
+		<div class="form-group password">
 		{{ Form::password('password' , [ 'placeholder' => 'Password', 'required']) }}
-		<br>
+		</div>
+
+		<div class="form-group repassword">
 		{{ Form::password('repassword' , [ 'placeholder' => 'Re Type Password', 'required']) }}
-		<br>
+		</div>
 
 		{{ Form:: submit('Sign Up') }}
 
 	{{ Form::close() }}
 
 </div>
+
 @stop
