6b8edfc02c2105aa01bba5274c844fcfa53eda4f
lee6346 <jvr632@my.utsa.edu>
2016-08-12 17:37:05 -0500

    added to tests

--- tests/functional/SearchCept.php
+++ tests/functional/SearchCept.php
@@ -4,6 +4,6 @@ $I->wantTo('Search for a movie');
 $user = Auth::user();
 $I->amOnPage('/feed');
 $I->fillField('input', '300');
-$I->click('Search');
+//$I->click('Search');
 
 //$I->amOnPage('/search');
--- tests/functional/SignUpCept.php
+++ tests/functional/SignUpCept.php
@@ -1,9 +1,7 @@
 <?php 
 $I = new FunctionalTester($scenario);
 $I->wantTo('Sign up for CinemaPhile');
-$I->amOnPage('/');
-$I->click('Sign Up for Free');
-$I->seeCurrentUrlEquals('/signup');
+$I->amOnPage('/signup');
 //fill all required input 
 $I->fillField('full_name', 'test');
 $I->fillField('username', 'test');
@@ -11,25 +9,20 @@ $I->fillField('email', 'test@gmail.com');
 $I->fillField('password', 'test');
 $I->fillField('repassword', 'test');
 $I->selectOption('gender', 'male');
-$I->grabTextFrom('select[name="day"] option:nth-child(2)');
-$->gradb
-/*
-$I->fillField('month', 'Jan');
-$I->fillField('year', 1980);
+$I->grabTextFrom('select option:nth-child(2)');
+$I->grabTextFrom('select option:nth-child(2)');
+$I->grabTextFrom('select option:nth-child(2)');
 $I->fillField('question', 'test');
 $I->fillField('answer', 'test');
+$I->click('Sign Up');
+
+//results 
+$I->amOnPage('/login');
+$I->seeRecord('users', ['email' => 'test@gmail.com']);
+
 
 //$I->seeCurrentUrlEquals('/login');
-
-//
-
 //submitForm()
-//click()
-//amOnPage()
-//wantTo()
-//fillField()
-//see("hello world)
-//seeIntDatabase('tablename', ['email' => 'mfdkd'])
-//lookForwardTo('being a member of the community)
 
-$I->seeRecord('users', ['email' => 'example@example.com']);
+
+
