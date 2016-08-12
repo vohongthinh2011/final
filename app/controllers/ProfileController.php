44493e44a1484997bf0ca54466e295f7e695fe39
Baraon Gallegos <gallegosbaraon@yahoo.com>
2016-08-12 18:28:29 -0500

    Reviews now showing on the profile page.

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -4,9 +4,11 @@ class ProfileController extends \BaseController{
     
     public function showProfile(){
         $user = Auth::user();
-        $user_reviews = Review::where('user_id' , '=', $user->id);
-        return View::make('profile', ['user' => $user,
-            'reviews' => $user_reviews]);
+        $user_reviews = Review::where('user_id' , '=', $user->id)->get();
+        return View::make('profile', [
+            'user' => $user,
+            'reviews' => $user_reviews
+            ]);
     }
     
     public function editProfile(){
